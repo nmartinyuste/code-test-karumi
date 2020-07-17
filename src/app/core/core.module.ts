b02b9bc36e5ef9930f4c146056a37349db674449
nataliaspotlab <natalia.martin@spotlab.org>
2020-07-17 17:24:15 +0200

    chore(core): add core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule, Optional, SkipSelf } from "@angular/core";
+
+@NgModule({
+  declarations: [],
+  imports: [],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() core: CoreModule) {
+    if (core) {
+      throw new Error("You should import core module only in the root module");
+    }
+  }
+}
