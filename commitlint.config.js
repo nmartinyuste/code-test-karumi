ad741e45879a6978ad1359b32737888d2eef5a5b
nataliaspotlab <natalia.martin@spotlab.org>
2020-07-17 18:06:52 +0200

    refactor(core): try commit scope mandatory (closes #1)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
-    'scope-empty': [1, 'never'],
+    'scope-empty': ['never'],
     'scope-enum': [2, 'always', ['core', 'styles', 'front-end', 'back-end', 'login']],
   },
 };
