a4393efd88bf3a0639e6ab65a1b2abbd249b5495
nataliaspotlab <natalia.martin@spotlab.org>
2020-07-20 12:37:47 +0200

    feat(back-end): add api login

---
+++ api/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Body parsers
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const auth = require('./routes/unauth/auth');
+app.use('/auth', auth);
+
+// 404 Not Found Errors
+app.use((req, res, next) => {
+  const err = new Error('Endpoint not Found');
+  err.status = 404;
+  next(err);
+});
+
+// 500 Internal Errors
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500);
+  res.send({
+    message: err.message || 'Undefined error',
+    errors: err.errors,
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
---
+++ api/fake/users.js
@@ -0,0 +1,22 @@
+const correctUsers = [
+  {
+    id: 1,
+    username: 'user1',
+    password: 'password1',
+    session_token: 'fakeSessionToken1',
+  },
+  {
+    id: 2,
+    username: 'user2',
+    password: 'password2',
+    session_token: 'fakeSessionToken2',
+  },
+  {
+    id: 3,
+    username: 'user3',
+    password: 'password3',
+    session_token: 'fakeSessionToken3',
+  },
+];
+
+module.exports = correctUsers;
